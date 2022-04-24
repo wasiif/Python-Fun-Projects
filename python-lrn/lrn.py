b01d90ba7c53d9e90b906a5e233812cbfddc091a
Wasif Ahmed <100182122+wasif-ahmd@users.noreply.github.com>
2022-04-24 18:11:00 +0530

    Create lrn.py

---
+++ python-lrn/lrn.py
@@ -0,0 +1,73 @@
+print("hello world!!!")
+
+# "" this is called string
+
+print("how are you?")
+
+print('hope you are doing well')
+
+print("im doing great thank you!")
+
+# multiline strings  
+
+print("im joey" + "im emily" + "im john")
+
+print("im joey " + "im emily " + "im john ")
+
+# \n is a new line indicator character.
+
+print("im ross \n" + "im captain america \n" + "im spiderman")
+
+# * is for mutliplication
+
+print("learning python" * 50)
+
+print("learning python\n" * 50)
+
+# input function 
+
+input("what is your name?")
+
+# variable is a method of storing data for reuse
+
+name = "joey"
+
+
+
+# drawing triangles
+
+print("|\\")
+print("| \\")
+print("|  \\")
+print("|   \\")
+print("|    \\")
+print("|_____\\")
+
+ 
+print("          /|")
+print("         / |")
+print("        /  |")
+print("       /   |")
+print("      /    |")
+print("     /     |")
+print("    /      |")
+print("   /       |")
+print("  /        |")
+print(" /         |")
+print("/__________|")
+
+
+
+# building a robot barista
+
+name = input("What is your Name? ")
+
+print("Hello " + name + ", Thank you so much for coming in today!")
+
+menu = "Black coffee, Espresso, Latte, Cappicino."
+
+print(name + ", What would you like from our Menu today?. Here is what we are serving,\n" + menu)
+
+order = input()
+
+print("Sounds good " + name + ", We'll have that " + order + " ready for you in a moment.")
